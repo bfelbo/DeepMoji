1ea4ceba47621e26978f8fde1b3a9f95781f4f62
rht <rhtbot@protonmail.com>
2017-11-07 23:31:54 +0100

    Bump Keras 2.0.7 -> 2.0.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'emoji==0.4.5',
         'h5py==2.7.0',
-        'Keras==2.0.7',
+        'Keras==2.0.9',
         'numpy==1.13.1',
         'scikit-learn==0.19.0',
         'text-unidecode==1.0',
