9b68b5daa12fff2d0cde4d52b507b69cac39caad
Nathan Klapstein <nklapste@ualberta.ca>
2020-01-09 11:17:33 -0700

    using `os.path.join` for globals path construction

--- deepmoji/global_variables.py
+++ deepmoji/global_variables.py
@@ -1,7 +1,7 @@
 """ Global variables.
 """
 import tempfile
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 
 # The ordering of these special tokens matter
 # blank tokens can be used for new purposes
@@ -16,8 +16,8 @@ SPECIAL_TOKENS = ['CUSTOM_MASK',
 SPECIAL_TOKENS.extend(['{}BLANK_{}'.format(SPECIAL_PREFIX, i) for i in range(6, 10)])
 
 ROOT_PATH = dirname(dirname(abspath(__file__)))
-VOCAB_PATH = '{}/model/vocabulary.json'.format(ROOT_PATH)
-PRETRAINED_PATH = '{}/model/deepmoji_weights.hdf5'.format(ROOT_PATH)
+VOCAB_PATH = join(ROOT_PATH, "model", "vocabulary.json")
+PRETRAINED_PATH = join(ROOT_PATH, "model", "deepmoji_weights.hdf5")
 
 WEIGHTS_DIR = tempfile.mkdtemp()
 
