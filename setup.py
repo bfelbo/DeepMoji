899587c0bb71c280acd32a2fe9dada8c147fab79
Nathan Klapstein <nklapste@ualberta.ca>
2020-01-10 10:44:20 -0700

    locking down requirements
    * allowing for a range of dependency versions, locked by api level

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ setup(
     description='DeepMoji library',
     include_package_data=True,
     install_requires=[
-        'emoji==0.4.5',
-        'h5py==2.7.0',
-        'Keras==2.0.9',
-        'numpy==1.13.1',
-        'scikit-learn==0.19.0',
-        'text-unidecode==1.0',
+        'emoji>=0.4.5,<1.0.0',
+        'h5py>=2.7.0,<3.0.0',
+        'Keras>=2.3.1,<3.0.0',
+        'numpy>=1.18.1,<2.0.0',
+        'scikit-learn>=0.19.0,<1.0.0',
+        'text-unidecode>=1.0,<2.0',
+    ],
+    tests_require=[
+        "nose>=1.3.7,<2.0.0",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
 )
