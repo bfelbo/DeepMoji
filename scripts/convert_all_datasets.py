3e2feba427815062c08e8980018a5b9cbf5185de
Nathan Klapstein <nklapste@ualberta.ca>
2020-01-10 08:45:07 -0700

    disabling loading of `PsychExp` dataset
    * encountering pickle load error: `_pickle.UnpicklingError: the STRING opcode argument must be quoted`

--- scripts/convert_all_datasets.py
+++ scripts/convert_all_datasets.py
@@ -16,7 +16,11 @@ from deepmoji.word_generator import WordGenerator
 
 DATASETS = [
     'Olympic',
-    'PsychExp',
+    # TODO: error loading this dataset's pickle
+    #   File ".../convert_all_datasets.py", line 81, in <module>
+    #       data = pickle.load(dataset, encoding='utf-8')
+    #   _pickle.UnpicklingError: the STRING opcode argument must be quoted
+    # 'PsychExp',
     'SCv1',
     'SCv2-GEN',
     'SE0714',
