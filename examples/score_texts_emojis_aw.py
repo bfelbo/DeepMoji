df72076fced51bac2f4fe04ce7b496da3c518391
John Punin <puninj@gmail.com>
2022-02-22 17:05:30 -0800

    Adding example to compute Emotional impact words

---
+++ examples/score_texts_emojis_aw.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+""" Use DeepMoji to score texts for emoji distribution.
+
+The resulting emoji ids (0-63) correspond to the mapping
+in emoji_overview.png file at the root of the DeepMoji repo.
+
+Compute the Attention Weights and report the most relevant 
+words with emotional impact
+
+Writes the result to a csv file.
+"""
+from __future__ import print_function, division
+import example_helper
+import json
+import csv
+import numpy as np
+from deepmoji.sentence_tokenizer import SentenceTokenizer
+from deepmoji.model_def import deepmoji_emojis
+from deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
+
+OUTPUT_PATH = 'test_sentences.csv'
+
+TEST_SENTENCES = [u'I love mom\'s cooking',
+                  u'I love how you never reply back..',
+                  u'I love cruising with my homies',
+                  u'I love messing with yo mind!!',
+                  u'I love you and now you\'re just gone..',
+                  u'This is shit',
+                  u'This is the shit']
+
+
+def top_elements(array, k):
+    ind = np.argpartition(array, -k)[-k:]
+    return ind[np.argsort(array[ind])][::-1]
+
+
+maxlen = 30
+batch_size = 32
+
+print('Tokenizing using dictionary from {}'.format(VOCAB_PATH))
+with open(VOCAB_PATH, 'r') as f:
+    vocabulary = json.load(f)
+st = SentenceTokenizer(vocabulary, maxlen)
+tokenized, _, _ = st.tokenize_sentences(TEST_SENTENCES)
+
+print('Loading model from {}.'.format(PRETRAINED_PATH))
+model = deepmoji_emojis(maxlen, PRETRAINED_PATH, return_attention=True)
+model.summary()
+
+print('Running predictions.')
+prob,awsen = model.predict(tokenized)
+
+# Compute Index to Word in the Vocabulary
+ind_to_word={ind: word for word,ind in vocabulary.iteritems()}
+
+# Find top emojis for each sentence. Emoji ids (0-63)
+# correspond to the mapping in emoji_overview.png
+# at the root of the DeepMoji repo.
+# Find the 5 top emotional impact words
+print('Writing results to {}'.format(OUTPUT_PATH))
+scores = []
+for i, t in enumerate(TEST_SENTENCES):
+    t_tokens = tokenized[i]
+    t_score = [t]
+    t_prob = prob[i]
+    ind_top = top_elements(t_prob, 5)
+    ind_awsen = top_elements(awsen[i],5)
+    top_words = [ind_to_word[t_tokens[ind]] for ind in ind_awsen]
+    t_score.append(sum(t_prob[ind_top]))
+    t_score.extend(ind_top)
+    t_score.extend([t_prob[ind] for ind in ind_top])
+    t_score.extend(top_words)
+    scores.append(t_score)
+    print(t_score)
+
+with open(OUTPUT_PATH, 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+    writer.writerow(['Text', 'Top5%',
+                     'Emoji_1', 'Emoji_2', 'Emoji_3', 'Emoji_4', 'Emoji_5',
+                     'Pct_1', 'Pct_2', 'Pct_3', 'Pct_4', 'Pct_5',
+                     'Word_1', 'Word_2', 'Word_3', 'Word_4','Word_5']])
+    for i, row in enumerate(scores):
+        try:
+            writer.writerow(row)
+        except Exception:
+            print("Exception at row {}!".format(i))
